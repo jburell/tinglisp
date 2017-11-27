19dd7f589e1503b64bf62aac2349623d96bb0957
Johan Burell <johan.burell@gmail.com>
2017-11-27 03:03:41 +0100

    Reworking logic. WIP.

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,134 @@
+#![feature(trace_macros)]
 #[macro_use]
 extern crate nom;
+extern crate either;
 
-use nom::alphanumeric;
+use nom::{alphanumeric, digit};
+use std::str::{self, FromStr};
+use std::collections::{HashMap, LinkedList};
 
-type Variable = String;
-type Filter = String;
-
-named! (plain_expression (&str) -> (Variable, Filter), do_parse! (
-    tag_s! ("(") >>
-    variable: alphanumeric >>
-    tag_s! ("|") >>
-    filter: alphanumeric >>
-    tag_s! (")") >>
-    ((variable.into(), filter.into()))));
-
-#[derive(Debug)]
-enum Expression {
-    Plain(Variable, Filter),
-    Recursive(Box<Expression>, Box<Expression>),
-    Left(Variable, Box<Expression>),
-    Right(Box<Expression>, Filter),
+#[derive(Debug, PartialEq)]
+pub enum TingValue {
+    Str(String),
+    Num(f32),
+    Array(Vec<TingValue>),
+    Object(HashMap<String, TingValue>),
+    List(LinkedList<TingValue>),
+    Value(),
 }
 
-//named! (recursive_expression (&str) -> Expression,
-//  alt_complete! (
-//    map! (plain_expression, |(v, f)| Expression::Plain (v, f)) |
-//    do_parse! (
-//      tag_s! ("(") >>
-//      sub: recursive_expression >>
-//      tag_s! ("|") >>
-//      filter: alphanumeric >>
-//      tag_s! (")") >>
-//      (Expression::Recursive (Box::new (sub), filter.into())))));
+// FIXME: since we already parsed a serie of digits and dots,
+// we know it is correct UTF-8. no need to use from_utf8 to
+// verify it is correct
+// FIXME: use alt_complete (implement ws for alt_complete)
+named!(
+    unsigned_float<f32>,
+    map_res!(
+        map_res!(
+            recognize!(alt_complete!(
+                delimited!(digit, tag!("."), opt!(complete!(digit)))
+                    | delimited!(opt!(digit), tag!("."), digit) | digit
+            )),
+            str::from_utf8
+        ),
+        FromStr::from_str
+    )
+);
 
-named! (recursive_expression (&str) -> Expression,
-    alt_complete! (
-        map! (plain_expression, |(v, f)| Expression::Plain (v, f)) |
-        do_parse! (
-            tag_s! ("(") >>
-            sub: recursive_expression >>
-            tag_s! ("|") >>
-            other: recursive_expression >>
-            tag_s! (")") >>
-            (Expression::Recursive (Box::new (sub), Box::new (other)))
-        ) |
-        do_parse! (
-            tag_s! ("(") >>
-            sub: alphanumeric >>
-            tag_s! ("|") >>
-            other: recursive_expression >>
-            tag_s! (")") >>
-            (Expression::Left (sub.into(), Box::new (other)))
-        ) |
-        do_parse! (
-            tag_s! ("(") >>
-            sub: recursive_expression >>
-            tag_s! ("|") >>
-            other: alphanumeric >>
-            tag_s! (")") >>
-            (Expression::Right (Box::new (sub), other.into()))
+named!(
+    float<f32>,
+    map!(
+        pair!(opt!(alt!(tag!("+") | tag!("-"))), unsigned_float),
+        |(sign, value): (Option<&[u8]>, f32)| {
+            sign.and_then(|s| {
+                if s[0] == ('-' as u8) {
+                    Some(-1f32)
+                } else {
+                    None
+                }
+            }).unwrap_or(1f32) * value
+        }
+    )
+);
+
+//FIXME: verify how json strings are formatted
+named!(
+    string<&str>,
+    delimited!(
+        tag!("\""),
+        map_res!(
+            escaped!(call!(alphanumeric), '\\', is_a!("\"n\\")),
+            str::from_utf8
+        ),
+        tag!("\"")
+    )
+);
+
+named!(
+    array<Vec<TingValue>>,
+    ws!(delimited!(
+        tag!("["),
+        separated_list!(tag!(","), value),
+        tag!("]")
+    ))
+);
+
+named!(
+    key_value<(&str, TingValue)>,
+    ws!(separated_pair!(string, tag!(":"), value))
+);
+
+named!(
+    hash<HashMap<String, TingValue>>,
+    ws!(map!(
+        delimited!(tag!("{"), separated_list!(tag!(","), key_value), tag!("}")),
+        |tuple_vec| {
+            let mut h: HashMap<String, TingValue> = HashMap::new();
+            for (k, v) in tuple_vec {
+                h.insert(String::from(k), v);
+            }
+            h
+        }
+    ))
+);
+
+named!(
+    list<LinkedList<TingValue>>,
+    ws!(map!(
+        delimited!(tag!("("), separated_list!(tag!(" "), key_value), tag!(")")),
+        |tuple_vec| {
+            let mut h: LinkedList<TingValue> = LinkedList::new();
+            for (k, v) in tuple_vec {
+                h.push_back(v);
+            }
+            h
+        }
+        ))
+);
+
+named!(
+    list_or_value<TingValue>,
+    ws!(
+        alt_complete!(
+            list  => { |l| list(l)   } |
+            value => { |v| value(v)  }
         )
     )
 );
 
+named!(
+    value<TingValue>,
+    ws!(dbg_dmp!(alt_complete!(
+      hash   => { |h|   TingValue::Object(h)            } |
+      array  => { |v|   TingValue::Array(v)             } |
+      string => { |s|   TingValue::Str(String::from(s)) } |
+      float  => { |num| TingValue::Num(num)             } |
+      list   => { |val| TingValue::List(val)            }
+    )))
+);
+
 fn main() {
-    let plain = "(var|fil)";
-    //let recursive = "(((var1|fil1)|fil2)|fil3)";
-    let recursive = "(((var1|fil1)|(var2|fil2))|(var3|fil3))";
-    let recursive2 = "((var1|(var2|fil2))|(var3|fil3))";
-    let recursive3 = "(((var1|fil1)|fil2)|(var3|fil3))";
-    println!("{:?}", plain_expression(plain));
-    println!("{:?}", recursive_expression(recursive));
-    println!("{:?}", recursive_expression(recursive2));
-    println!("{:?}", recursive_expression(recursive3));
+    let code = &b"(conj (conj #{1 2 3} 4) 4)";
+    let result = value(&code[..]);
+    println!("{:?}", result);
 }
